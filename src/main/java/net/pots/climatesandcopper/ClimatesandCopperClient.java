aceb07ba39a67ad37473efdbc88d8bdc81a73294
PotatoesInTheSky1 <sonnymadams@gmail.com>
2023-10-28 23:16:14 +0100

    xds

---
+++ src/main/java/net/pots/climatesandcopper/ClimatesandCopperClient.java
@@ -0,0 +1,10 @@
+package net.pots.climatesandcopper;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClimatesandCopperClient implements ClientModInitializer  {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
